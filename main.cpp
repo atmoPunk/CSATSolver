ef1f2c19621738a4f0b4c291b01f2edfc744564a
Dmitriy Ivanov <atmopunk@outlook.com>
2021-09-19 13:58:59 +0300

    Fix solver

--- main.cpp
+++ main.cpp
@@ -2,20 +2,36 @@
 #include <fstream>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 #include <regex>
 
 #include <cadical.hpp>
 
-const std::regex INPUT_VARIABLE(R"(INPUT\((\w+)\))");
-const std::regex OUTPUT_VARIABLE(R"(OUTPUT\((\w+)\))");
-const std::regex AND(R"((\w+) = AND\((\w+), (\w+)\))");
-const std::regex NAND(R"((\w+) = NAND\((\w+), (\w+)\))");
-const std::regex OR(R"((\w+) = OR\((\w+), (\w+)\))");
-const std::regex NOR(R"((\w+) = NOR\((\w+), (\w+)\))");
-const std::regex NOT(R"((\w+) = NOT\((\w+)\))");
-const std::regex XOR(R"((\w+) = XOR\((\w+), (\w+)\))");
-const std::regex XNOR(R"((\w+) = XNOR\((\w+), (\w+)\))");
-const std::regex BIN_OP(R"((\w+) = (AND|NAND|OR|NOR|XOR|XNOR)\((\w+), (\w+)\))");
+const std::regex INPUT_VARIABLE(R"(\s*INPUT\s*\(\s*(\w+)\s*\)\s*)");
+const std::regex OUTPUT_VARIABLE(R"(\s*OUTPUT\s*\(\s*(\w+)\s*\)\s*)");
+const std::regex NOT(R"(\s*(\w+)\s*=\s*NOT\s*\((\w+)\))");
+const std::regex BIN_OP(R"(\s*(\w+)\s*=\s*(AND|NAND|OR|NOR|XOR|NXOR)\s*\(\s*((\w+)\s*,\s*)+(\w+)\s*\)\s*)");
+const std::regex NAME(R"(\w+)");
+
+void comb(const std::vector<int>& input, std::vector<int>& combination, int offset, int k, CaDiCaL::Solver& solver) {
+  if (k == 0) {
+    for (int i = 0; i < input.size(); ++i) {
+        if (combination[i] == 0) {
+            solver.add(input[i]);
+        } else {
+            solver.add(-input[i]);
+        }
+    }
+    solver.add(0);
+    return;
+  }
+  for (int i = offset; i <= input.size() - k; ++i) {
+    combination[i] = 1;
+    comb(input, combination, i + 1, k - 1, solver);
+    combination[i] = 0;
+  }
+}
+
 
 int getOrInsert(std::unordered_map<std::string, int>& map, std::string&& s) {
     if (!map.contains(s)) {
@@ -26,33 +42,77 @@ int getOrInsert(std::unordered_map<std::string, int>& map, std::string&& s) {
     }
 }
 
-void binOp(std::string&& op, int res, int left, int right, CaDiCaL::Solver& solver) {
+void and_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    for (int var : variables) {
+        solver.add(var); solver.add(-res); solver.add(0);
+    }
+    for (int var : variables) {
+        solver.add(-var);
+    }
+    solver.add(res); solver.add(0);
+}
+
+void nand_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    for (int var : variables) {
+        solver.add(var); solver.add(res); solver.add(0);
+    }
+    for (int var : variables) {
+        solver.add(-var);
+    }
+    solver.add(-res); solver.add(0);
+}
+
+void or_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    for (int var : variables) {
+        solver.add(-var); solver.add(res); solver.add(0);
+    }
+    for (int var : variables) {
+        solver.add(var);
+    }
+    solver.add(-res); solver.add(0);
+}
+
+void nor_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    for (int var : variables) {
+        solver.add(-var); solver.add(-res); solver.add(0);
+    }
+    for (int var : variables) {
+        solver.add(var);
+    }
+    solver.add(res); solver.add(0);
+}
+
+void xor_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    variables.push_back(res);
+    std::vector<int> combi(variables.size(), 0);
+    for (int i = 1; i <= variables.size(); i += 2) {
+        comb(variables, combi, 0, i, solver);
+    }
+}
+
+void nxor_op(int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
+    variables.push_back(res);
+    std::vector<int> combi(variables.size(), 0);
+    for (int i = 0; i <= variables.size(); i += 2) {
+        comb(variables, combi, 0, i, solver);
+    }
+}
+
+void binOp(const std::string& op, int res, std::vector<int>&& variables, CaDiCaL::Solver& solver) {
     if (op == "AND") {
-        solver.add(-left); solver.add(-right); solver.add(res); solver.add(0);
-        solver.add(left); solver.add(-res); solver.add(0);
-        solver.add(right); solver.add(-res); solver.add(0);
+        and_op(res, std::move(variables), solver);
     } else if (op == "NAND") {
-        solver.add(-left); solver.add(-right); solver.add(-res); solver.add(0);
-        solver.add(left); solver.add(res); solver.add(0);
-        solver.add(right); solver.add(res); solver.add(0);
+        nand_op(res, std::move(variables), solver);
     } else if (op == "OR") {
-        solver.add(left); solver.add(right); solver.add(-res); solver.add(0);
-        solver.add(-left); solver.add(res); solver.add(0);
-        solver.add(-right); solver.add(res); solver.add(0);
+        or_op(res, std::move(variables), solver);
     } else if (op == "NOR") {
-        solver.add(left); solver.add(right); solver.add(res); solver.add(0);
-        solver.add(-left); solver.add(-res); solver.add(0);
-        solver.add(-right); solver.add(-res); solver.add(0);
+        nor_op(res, std::move(variables), solver);
     } else if (op == "XOR") {
-        solver.add(-left); solver.add(-right); solver.add(-res); solver.add(0);
-        solver.add(left); solver.add(right); solver.add(-res); solver.add(0);
-        solver.add(left); solver.add(-right); solver.add(res); solver.add(0);
-        solver.add(-left); solver.add(right); solver.add(res); solver.add(0);
-    } else if (op == "XNOR") {
-        solver.add(left); solver.add(right); solver.add(res); solver.add(0);
-        solver.add(left); solver.add(-right); solver.add(-res); solver.add(0);
-        solver.add(-left); solver.add(-right); solver.add(res); solver.add(0);
-        solver.add(-left); solver.add(right); solver.add(-res); solver.add(0);
+        xor_op(res, std::move(variables), solver);
+    } else if (op == "NXOR") {
+        nxor_op(res, std::move(variables), solver);
+    } else {
+        std::cerr << "Unknown Op " << op << std::endl;
     }
 }
 
@@ -88,15 +148,21 @@ int main(int argc, char* argv[]) {
             int res = getOrInsert(name_to_idx, std::move(variable));
             solver.assume(res);
         } else if (std::regex_match(line, match, BIN_OP)) {
-            std::string res_var = match[1].str();
-            std::string left_var = match[3].str();
-            std::string right_var = match[4].str();
-            int res = getOrInsert(name_to_idx, std::move(res_var));
-            int left = getOrInsert(name_to_idx, std::move(left_var));
-            int right = getOrInsert(name_to_idx, std::move(right_var));
-
+            int res = getOrInsert(name_to_idx, match[1].str());
             std::string op = match[2].str();
-            binOp(std::move(op), res, left, right, solver);
+            std::vector<int> variables;
+            std::smatch name_match;
+            int toSkip = 2;
+            while (std::regex_search(line, name_match, NAME)) {
+                if (toSkip > 0) {
+                    line = name_match.suffix();
+                    toSkip--;
+                    continue;
+                }
+                variables.push_back(getOrInsert(name_to_idx, name_match[0].str()));
+                line = name_match.suffix();
+            }
+            binOp(op, res, std::move(variables), solver);
         } else if (std::regex_match(line, match, NOT)) {
             std::string res_var = match[1].str();
             std::string in_var = match[2].str();
@@ -104,6 +170,9 @@ int main(int argc, char* argv[]) {
             int in = getOrInsert(name_to_idx, std::move(in_var));
             solver.add(-res); solver.add(-in); solver.add(0);
             solver.add(res); solver.add(in); solver.add(0);
+        } else {
+            std::cerr << "Can not parse line: " << line << std::endl;
+            return 3;
         }
     }
 
